34d62505e67558d261c05ba14678ec57dd10418e
rhkwj <rhkwj@naver.com>
2018-04-22 10:42:08 +0800

    Add the project

---
+++ Auto Layout Calulater/Auto Layout Calulater/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Auto Layout Calulater
+//
+//  Created by Kim Yeon Jeong on 2018/4/16.
+//  Copyright © 2018 Kim Yeon Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
